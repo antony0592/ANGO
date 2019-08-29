c8da84db72ec373afcc804c21e3d8beda767bb0f
GabrielN0234 <49532310+GabrielN0234@users.noreply.github.com>
2019-08-29 06:25:53 +0200

    Add files via upload

---
+++ backendtienda/DBConnection.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backendtienda;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author loin1
+ */
+public class DBConnection {
+    public DBConnection(){
+        
+    }
+    
+    public void connect(){
+        JOptionPane.showMessageDialog(null,"no data base connection :( ");
+    }
+}
